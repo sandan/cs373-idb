52eaff5e6aff70d2fb2a99ac0ec1eba2ef68f991
Kyle Nicola <kyle.a.nicola@gmail.com>
2014-07-05 18:33:15 -0500

    created the models for our app

---
+++ models.py
@@ -0,0 +1,43 @@
+
+from django.db import models
+
+# Create your models here.
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=400)
+    sponsor_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+    business_type = models.CharField(max_length=400)
+    website = models.CharField(max_length=400)
+    def __unicode__(self):
+        return self.name
+
+class Stage(models.Model):
+    name = models.CharField(max_length=400)
+    sponsor = models.OneToOneField(Sponsor)
+    def __unicode__(self):
+        return self.name
+
+class Artist(models.Model):
+    name = models.CharField(max_length=400)
+    label = models.CharField(max_length=400)
+    origin = models.CharField(max_length=400)
+    website = models.CharField(max_length=400)
+    genre = models.CharField(max_length=400)
+    stage = models.ForeignKey(Stage)
+    def __unicode__(self):
+        return self.name
+
+class Band_Photo(models.Model):
+    artist_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+    file_name = models.CharField(max_length=400)
+    artist_id = models.ForeignKey(Artist)
+    def __unicode__(self):
+        return self.file_name
+
+class Band_Member(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    band = models.ForeignKey(Artist)
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
+
