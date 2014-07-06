6634426c643fd98503e51fb1405c8d0cec647a6b
Kyle Nicola <kyle.a.nicola@gmail.com>
2014-07-05 20:22:24 -0500

    renamed things, changed stage/sponsor relationship

--- models.py
+++ models.py
@@ -1,19 +1,20 @@
 
 from django.db import models
 
-# Create your models here.
-
-class Sponsor(models.Model):
-    name = models.CharField(max_length=400, unique=True)
-    #sponsor_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
-    business_type = models.CharField(max_length=400)
-    website = models.CharField(max_length=400)
-    def __str__(self):
-        return self.name
 
 class Stage(models.Model):
     name = models.CharField(max_length=400, unique=True)
-    sponsor = models.OneToOneField(Sponsor)
+
+    def __str__(self):
+        return self.name
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=400)
+    #sponsor_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+    business_type = models.CharField(max_length=400)
+    website = models.CharField(max_length=400)
+    stage = models.ForeignKey(Stage, blank=True, null=True)
+
     def __str__(self):
         return self.name
 
@@ -24,20 +25,26 @@ class Artist(models.Model):
     website = models.CharField(max_length=400)
     genre = models.CharField(max_length=400)
     stage = models.ForeignKey(Stage)
+
     def __str__(self):
         return self.name
 
-class Band_Photo(models.Model):
+class Artist_Photo(models.Model):
     #artist_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
-    file_name = models.CharField(max_length=400, unique=True)
-    artist_id = models.ForeignKey(Artist)
+    file_name = models.CharField(max_length=400)
+    artist = models.ForeignKey(Artist)
+
     def __str__(self):
         return self.file_name
 
-class Band_Member(models.Model):
+class Artist_Member(models.Model):
     first_name = models.CharField(max_length=400)
     last_name = models.CharField(max_length=400)
-    band = models.ForeignKey(Artist)
+    artist = models.ForeignKey(Artist)
+
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        ret_str = first_name
+        if(last_name):
+            ret_str += " " + last_name
+        return ret_str
 
