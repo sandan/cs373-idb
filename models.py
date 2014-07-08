fe669d1380df3b5a6828b369d7b85c45bc3132d8
Carlos A. Rodriguez <car3564@utexas.edu>
2014-07-07 19:50:46 -0500

    Cleaned up some code in models.py so it looks more organized and it's easier to read

--- models.py
+++ models.py
@@ -3,44 +3,44 @@ from django.db import models
 
 
 class Stage(models.Model):
-    name = models.CharField(max_length=400, unique=True)
+    name            = models.CharField(max_length=400, unique=True)
 
     def __str__(self):
         return self.name
 
 class Sponsor(models.Model):
-    name = models.CharField(max_length=400)
-    #sponsor_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
-    business_type = models.CharField(max_length=400)
-    website = models.CharField(max_length=400)
-    stage = models.ForeignKey(Stage, blank=True, null=True)
+    name            = models.CharField(max_length=400)
+    #sponsor_pic    = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+    business_type   = models.CharField(max_length=400)
+    website         = models.CharField(max_length=400)
+    stage           = models.ForeignKey(Stage, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Artist(models.Model):
-    name = models.CharField(max_length=400, unique=True)
-    label = models.CharField(max_length=400)
-    origin = models.CharField(max_length=400)
-    website = models.CharField(max_length=400)
-    genre = models.CharField(max_length=400)
-    stage = models.ForeignKey(Stage)
+    name            = models.CharField(max_length=400, unique=True)
+    label           = models.CharField(max_length=400)
+    origin          = models.CharField(max_length=400)
+    website         = models.CharField(max_length=400)
+    genre           = models.CharField(max_length=400)
+    stage           = models.ForeignKey(Stage)
 
     def __str__(self):
         return self.name
 
 class ArtistPhoto(models.Model):
-    #artist_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
-    file_name = models.CharField(max_length=400)
-    artist = models.ForeignKey(Artist)
+    #artist_pic     = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+    file_name       = models.CharField(max_length=400)
+    artist          = models.ForeignKey(Artist)
 
     def __str__(self):
         return self.file_name
 
 class ArtistMember(models.Model):
-    first_name = models.CharField(max_length=400)
-    last_name = models.CharField(max_length=400)
-    artist = models.ForeignKey(Artist)
+    first_name      = models.CharField(max_length=400)
+    last_name       = models.CharField(max_length=400)
+    artist          = models.ForeignKey(Artist)
 
     def __str__(self):
         ret_str = self.first_name
