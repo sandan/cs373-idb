78335601d5636b13f1c31d2d695db2a1d4ca6f23
Mark Sandan <msandan@utexas.edu>
2014-07-22 20:14:31 -0500

    Update models.py

--- mysite/cs373/models.py
+++ mysite/cs373/models.py
@@ -32,7 +32,7 @@ class Sponsor(models.Model):
     name            = models.CharField(max_length=400)
     #sponsor_pic    = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
     business_type   = models.CharField(max_length=400)
-    website         = models.CharField(max_length=400)
+    #website         = models.CharField(max_length=400)
     stage           = models.ForeignKey(Stage, blank=True, null=True)
 
 
@@ -65,10 +65,10 @@ class Artist(models.Model):
     name            = models.CharField(max_length=400, unique=True)
     label           = models.CharField(max_length=400)
     origin          = models.CharField(max_length=400)
-    website         = models.CharField(max_length=400)
-    genre           = models.CharField(max_length=400)
-    bio             = models.TextField(max_length=5000)
-    youtube         = models.CharField(max_length=400)
+    #website         = models.CharField(max_length=400)
+     genre           = models.CharField(max_length=400)
+    #bio             = models.TextField(max_length=5000)
+    #youtube         = models.CharField(max_length=400)
     stage           = models.ForeignKey(Stage)
 
     def get_url(self):
