eb92b8d5ac0b0cbf0cd4b631f0505b99751ca321
Kyle Nicola <kyle.a.nicola@gmail.com>
2014-07-30 17:47:00 -0500

    updating serializer to include ids

--- mysite/cs373/serializers.py
+++ mysite/cs373/serializers.py
@@ -4,29 +4,29 @@ from cs373 import models
 class StageSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.Stage
-		fields = ('name',)
+		fields = ('id', 'name')
 
 class SponsorSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.Sponsor
-		fields = ('name', 'business_type', 'stage')
+		fields = ('id', 'name', 'business_type', 'stage')
 
 class ArtistSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.Artist
-		fields = ('name', 'label', 'origin', 'genre', 'stage')
+		fields = ('id', 'name', 'label', 'origin', 'genre', 'stage')
 
 class ArtistMediaSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.ArtistMedia
-		fields = ('bio', 'photo', 'youtube', 'video', 'twitter', 'facebook', 'twitterwidget', 'youtubevideo', 'webpage')
+		fields = ('ar','bio', 'photo', 'youtube', 'video', 'twitter', 'facebook', 'twitterwidget', 'youtubevideo', 'webpage')
 
 class StageMediaSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.StageMedia
-		fields = ('bio', 'photo', 'youtube', 'video', 'twitter', 'facebook', 'twitterwidget', 'youtubevideo', 'webpage')
+		fields = ('st', 'bio', 'photo', 'youtube', 'video', 'twitter', 'facebook', 'twitterwidget', 'youtubevideo', 'webpage')
 
 class SponsorMediaSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.SponsorMedia
-		fields = ('bio', 'photo', 'youtube', 'video', 'twitter', 'facebook', 'twitterwidget', 'youtubevideo', 'webpage')
+		fields = ('sp', 'bio', 'photo', 'youtube', 'video', 'twitter', 'facebook', 'twitterwidget', 'youtubevideo', 'webpage')
