f05da8f0e01a2ce5f71ab242c60da0f4731c6fb2
Kyle Nicola <kyle.a.nicola@gmail.com>
2014-07-30 23:54:51 -0500

    Adding admin section for the models

--- mysite/cs373/admin.py
+++ mysite/cs373/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from cs373.models import *
 # Register your models here.
+
+class StageMediaInline(admin.StackedInline):
+    model = StageMedia
+    extra = 0
+
+class StageAdmin(admin.ModelAdmin):
+    inlines = [StageMediaInline]
+
+class SponsorMediaInline(admin.StackedInline):
+    model = SponsorMedia
+    extra = 0
+
+class SponsorAdmin(admin.ModelAdmin):
+    inlines = [SponsorMediaInline]
+
+class ArtistMediaInline(admin.StackedInline):
+    model = ArtistMedia
+    extra = 0
+
+class ArtistAdmin(admin.ModelAdmin):
+    inlines = [ArtistMediaInline]
+
+
+admin.site.register(Stage, StageAdmin)
+admin.site.register(Sponsor, SponsorAdmin)
+admin.site.register(Artist, ArtistAdmin)
