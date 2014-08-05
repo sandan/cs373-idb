7eba384bff40cdad3271fdc252c4c0b23f57404c
Stephen Ridings <SRidings777@aol.com>
2014-08-04 21:59:13 -0500

    Updated load stages
    Updated load stages to parse data for database

--- mysite/cs373/management/commands/load_stages.py
+++ mysite/cs373/management/commands/load_stages.py
@@ -18,25 +18,34 @@ class Command(BaseCommand):
             stage_dict=dict()
             stage_media_dict=dict()
             current_stage=None
+            y = line.split(': ')
+            d = y[2].split(', ')
+            
+            year = d[0]
+            month = d[1]
+            day = d[2]
+            
+            date = datetime.date(year,month,day)
+            
             for line in F:
                 if (line == "\n"):
                     #new stage+media
+                    current_stage=create_stage(stage_dict)
                     stage_dict.clear()
-                    self.create_stage_media(stage_media_dict,current_stage)
+                    self.create_stage_media(stage_media_dict,current_stage, date)
                     stage_media_dict.clear()
 
                 else:
                     info=line.split(': ')
                     assert (len(info)==2)
-
-                    if (info[0].lower() == 'name'):
-                        stage_dict[info[0].lower()]=info[1].strip()
-                        current_stage=self.create_stage(stage_dict)
-
+                    key=info[0].lower()
+                    val=info[1].strip()
+                    
+                    if (key == 'location'):
+                        stage_dict[key]=val
+                    
                     else:
-                        if (info[1].lower() == 'null\n'):
-                            continue
-                        stage_media_dict[info[0].lower()]=info[1].strip()
+                        stage_media_dict[key]=val
 
 
     def create_stage(self, data):
@@ -46,10 +55,10 @@ class Command(BaseCommand):
         self.stdout.write("+created: stage: "+data['name'])
         return s
 
-    def create_stage_media(self, stage_data, stage):
+    def create_stage_media(self, stage_data, stage, date):
 
         sm=StageMedia(**stage_data)
-        sm.st=stage
-        stage.save()
+        sm.stage=stage
+        sm.year = date
         sm.save()
         self.stdout.write("+created: "+ stage.name)
