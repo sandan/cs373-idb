c3c7d6623027d2023cbb7f7783a4309a49192697
jh6779 <jesushernandez@utexas.edu>
2014-08-06 18:13:06 -0500

    Update urls.py to use haystack urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^search/', include('haystack.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cs373.urls')),
 )
